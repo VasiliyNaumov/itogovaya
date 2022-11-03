d3563d1786051f642669c8f2469e76b188e14d11
VAsiliy <vasyannaumov@gmail.com>
2022-11-03 22:23:07 +0300

    окончательный вариант программы

--- program.cs
+++ program.cs
@@ -17,3 +17,22 @@ string[] DelElements(string[] text)
     string[] arrayText = newText.Split(' '); 
     return arrayText; 
 } 
+Console.WriteLine("Введите текст и нажмите клавишу Enter"); 
+Console.WriteLine("Если нажать Enter не вводя текст, то программа запустит встроенный текст"); 
+Console.WriteLine(); 
+string enter = Console.ReadLine(); 
+if (enter == String.Empty) 
+{ 
+    string[] text = { "СМИ", "сообщили,", "что", "Илон", "Маск", "договорился", "с", "международными", "инвесторами", "о", "предоставлении", "ему", "$13", "млрд:", "заёмных", "средств", "на", "покупку", "Twitter" }; 
+    Console.WriteLine(PrintText(text)); 
+    Console.WriteLine(); 
+    string[] arrayText = DelElements(text); 
+    Console.WriteLine(PrintText(arrayText)); 
+} 
+else 
+{ 
+    Console.WriteLine(); 
+    string[] text = enter.Split(' '); 
+    string[] arrayText = DelElements(text); 
+    Console.WriteLine(PrintText(arrayText)); 
+}
